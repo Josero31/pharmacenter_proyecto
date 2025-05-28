7e12055a514c28f92e8e44010fbbca055c92fea7
pablonyaaUwu2022 <pabyiart28@gmail.com>
2025-05-27 19:08:33 -0600

    añadir searchbar al header

--- Scrum/frontend/src/components/Header.jsx
+++ Scrum/frontend/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import SearchBar from './SearchBar'
 
 function Header() {
   return (
     <header className="header">
       <button id="openSidebar">&#9776;</button>
       <h1>Farmacia Bethesda</h1>
+      <SearchBar />
     </header>
   )
 }
